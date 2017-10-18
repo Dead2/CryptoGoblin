01996e7d1ed1e0e61e94390133d1aeaa16a0c694
Hans Kristian Rosbach <hk-git@circlestorm.org>
2017-10-18 12:55:10 +0200

    Cleanup sbox in soft-aes, based on fireice_uk's changes. Optimize alignment for caceline sizes.

--- crypto/cryptonight_aesni.h
+++ crypto/cryptonight_aesni.h
@@ -51,10 +51,10 @@ extern "C"
 	void keccak(const uint8_t *in, int inlen, uint8_t *md, int mdlen);
 	void keccakf(uint64_t st[25], int rounds);
 	extern void(*const extra_hashes[4])(const void *, size_t, char *);
-
-	__m128i soft_aesenc(__m128i in, __m128i key);
-	__m128i soft_aeskeygenassist(__m128i key, uint8_t rcon);
 }
+
+__m128i soft_aesenc(__m128i in, __m128i key);
+__m128i soft_aeskeygenassist(__m128i key, uint8_t rcon);
 
 // This will shift and xor tmp1 into itself as 4 32-bit vals such as
 // sl_xor(a1 a2 a3 a4) = a1 (a2^a1) (a3^a2^a1) (a4^a3^a2^a1)
