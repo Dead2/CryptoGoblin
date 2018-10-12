psychocrypt/xmr-stak

CPU: change default for `asm`
Remove the asm option `auto` by `off`

xmrstak/backend/cpu/autoAdjust.hpp
@@ -82,7 +82,7 @@ public:
 
 				conf += std::string("    { \"low_power_mode\" : ");
 				conf += std::string(double_mode ? "true" : "false");
-				conf += std::string(", \"no_prefetch\" : true, \"asm\" : \"auto\", \"affine_to_cpu\" : ");
+				conf += std::string(", \"no_prefetch\" : true, \"asm\" : \"off\", \"affine_to_cpu\" : ");
 				conf += std::to_string(aff_id);
 				conf += std::string(" },\n");
 

xmrstak/backend/cpu/autoAdjustHwloc.hpp
@@ -70,7 +70,7 @@ public:
 			{
 				conf += std::string("    { \"low_power_mode\" : ");
 				conf += std::string((id & 0x8000000) != 0 ? "true" : "false");
-				conf += std::string(", \"no_prefetch\" : true,  \"asm\" : \"auto\", \"affine_to_cpu\" : ");
+				conf += std::string(", \"no_prefetch\" : true,  \"asm\" : \"off\", \"affine_to_cpu\" : ");
 				conf += std::to_string(id & 0x7FFFFFF);
 				conf += std::string(" },\n");
 			}

xmrstak/backend/cpu/minethd.hpp
@@ -32,7 +32,7 @@ public:
 private:
 
 	template<size_t N>
-	static cn_hash_fun func_multi_selector(bool bHaveAes, bool bNoPrefetch, xmrstak_algo algo, const std::string& asm_version_str = "auto");
+	static cn_hash_fun func_multi_selector(bool bHaveAes, bool bNoPrefetch, xmrstak_algo algo, const std::string& asm_version_str = "off");
 
 	minethd(miner_work& pWork, size_t iNo, int iMultiway, bool no_prefetch, int64_t affinity, const std::string& asm_version);
 
@@ -60,7 +60,7 @@ private:
 
 	bool bQuit;
 	bool bNoPrefetch;
-	std::string asm_version_str = "auto";
+	std::string asm_version_str = "off";
 };
 
 } // namespace cpu

Dead2/CryptoGoblin

xmrstak/backend/cpu/autoAdjust.hpp
@@ -81,7 +81,7 @@ public:
 
                 conf += std::string("    { \"thread_mode\" : ");
                 conf += std::string(double_mode ? "2" : "1");
-                conf += std::string(", \"prefetch\" : true, \"asm\" : \"auto\", \"affine_to_cpu\" : ");
+                conf += std::string(", \"prefetch\" : true, \"asm\" : \"off\", \"affine_to_cpu\" : ");
                 conf += std::to_string(aff_id);
                 conf += std::string(" },\n");
 

xmrstak/backend/cpu/autoAdjustHwloc.hpp
@@ -69,7 +69,7 @@ public:
             {
                 conf += std::string("    { \"thread_mode\" : ");
                 conf += std::string((id & 0x8000000) != 0 ? "2" : "1");
-                conf += std::string(", \"prefetch\" : true,  \"asm\" : \"auto\", \"affine_to_cpu\" : ");
+                conf += std::string(", \"prefetch\" : true,  \"asm\" : \"off\", \"affine_to_cpu\" : ");
                 conf += std::to_string(id & 0x7FFFFFF);
                 conf += std::string(" },\n");
             }

xmrstak/backend/cpu/minethd.hpp
@@ -31,7 +31,7 @@ public:
 
 private:
     template<size_t N>
-    static cn_hash_fun func_multi_selector(bool bHaveAes, bool bPrefetch, xmrstak_algo algo, const std::string& asm_version_str = "auto");
+    static cn_hash_fun func_multi_selector(bool bHaveAes, bool bPrefetch, xmrstak_algo algo, const std::string& asm_version_str = "off");
 
     minethd(miner_work& pWork, size_t iNo, int iMultiway, bool prefetch, int64_t affinity, const std::string& asm_version);
 
@@ -59,7 +59,7 @@ private:
 
     bool bQuit;
     bool bPrefetch;
-    std::string asm_version_str = "auto";
+    std::string asm_version_str = "off";
 };
 
 } // namespace cpu