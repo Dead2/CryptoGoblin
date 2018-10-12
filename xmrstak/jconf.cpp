c751b67110aefc024dcef230917daa8b8bd6e1f0
Hans Kristian Rosbach <hk-git@circlestorm.org>
2018-10-12 20:27:04 +0200

    Change BBScoin POW for v4

--- xmrstak/jconf.cpp
+++ xmrstak/jconf.cpp
@@ -90,7 +90,7 @@ xmrstak::coin_selection coins[] = {
     // {newalgo, oldalgo, fromversion}
 #ifndef ONLY_XMR_ALGO
     { "aeon7",                      {cryptonight_aeon,      cryptonight_lite,       7u },  {cryptonight_aeon,       cryptonight_aeon,       0u },   "mine.aeon-pool.com:5555" },
-    { "bbscoin",                    {cryptonight_lite,      cryptonight_monero,     4u },  {cryptonight_monero,     cryptonight_monero,     0u },   nullptr },
+    { "bbscoin",                    {cryptonight_aeon,      cryptonight_monero,     4u },  {cryptonight_monero,     cryptonight_monero,     0u },   nullptr },
     { "croat",                      {cryptonight_monero,    cryptonight,            0u },  {cryptonight_monero,     cryptonight_monero,     0u },   nullptr },
     { "cryptonight",                {cryptonight,           cryptonight,            0u },  {cryptonight_monero,     cryptonight_monero,     0u },   nullptr },
     { "cryptonight_heavy",          {cryptonight_heavy,     cryptonight_heavy,      0u },  {cryptonight_heavy,      cryptonight_heavy,      0u },   nullptr },
