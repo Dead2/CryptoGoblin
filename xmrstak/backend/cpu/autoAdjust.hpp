fireice-uk/xmr-stak

add cpu family and model detection
Helper functions to select the asm version based on the number of used hashes per threads and the family name of the cpu.

- use the noew cpu type functions to fix the wrong AMD family detection in `autoAdjust.hpp`
- allow to set the asm version to `auto`
- rename asm option `intel` to `intel_avx`
- rename asm option `ryzen` to `amd_avx`

Co-authored-by: fireice-uk <fireice-uk@users.noreply.github.com>

xmrstak/backend/cpu/autoAdjust.hpp
@@ -7,6 +7,7 @@
 #include "xmrstak/misc/configEditor.hpp"
 #include "xmrstak/params.hpp"
 #include "xmrstak/backend/cryptonight.hpp"
+#include "xmrstak/backend/cpu/cpuType.hpp"
 #include <string>
 
 #ifdef _WIN32
@@ -20,14 +21,6 @@ namespace xmrstak
 {
 namespace cpu
 {
-// Mask bits between h and l and return the value
-// This enables us to put in values exactly like in the manual
-// For example EBX[31:22] is get_masked(cpu_info[1], 31, 22)
-inline int32_t get_masked(int32_t val, int32_t h, int32_t l)
-{
-	val &= (0x7FFFFFFF >> (31-(h-l))) << l;
-	return val >> l;
-}
 
 class autoAdjust
 {
@@ -82,7 +75,7 @@ public:
 
 				conf += std::string("    { \"low_power_mode\" : ");
 				conf += std::string(double_mode ? "true" : "false");
-				conf += std::string(", \"no_prefetch\" : true, \"asm\" : \"off\", \"affine_to_cpu\" : ");
+				conf += std::string(", \"no_prefetch\" : true, \"asm\" : \"auto\", \"affine_to_cpu\" : ");
 				conf += std::to_string(aff_id);
 				conf += std::string(" },\n");
 
@@ -143,7 +136,8 @@ private:
 			L3KB_size = get_masked(cpu_info[3], 31, 18) * 512;
 
 			::jconf::cpuid(1, 0, cpu_info);
-			if(get_masked(cpu_info[0], 11, 8) < 0x17) //0x17h is Zen
+
+			if(getModel().family < 0x17) //0x17h is Zen
 				old_amd = true;
 
 			return true;

xmrstak/backend/cpu/autoAdjustHwloc.hpp
@@ -70,7 +70,7 @@ public:
 			{
 				conf += std::string("    { \"low_power_mode\" : ");
 				conf += std::string((id & 0x8000000) != 0 ? "true" : "false");
-				conf += std::string(", \"no_prefetch\" : true, \"asm\" : \"off\", \"affine_to_cpu\" : ");
+				conf += std::string(", \"no_prefetch\" : true, \"asm\" : \"auto\", \"affine_to_cpu\" : ");
 				conf += std::to_string(id & 0x7FFFFFF);
 				conf += std::string(" },\n");
 			}

Dead2/CryptoGoblin

xmrstak/backend/cpu/autoAdjust.hpp
@@ -7,6 +7,7 @@
 #include "xmrstak/misc/configEditor.hpp"
 #include "xmrstak/params.hpp"
 #include "xmrstak/backend/cryptonight.hpp"
+#include "xmrstak/backend/cpu/cpuType.hpp"
 #include <string>
 
 #ifdef _WIN32
@@ -20,14 +21,6 @@ namespace xmrstak
 {
 namespace cpu
 {
-// Mask bits between h and l and return the value
-// This enables us to put in values exactly like in the manual
-// For example EBX[31:22] is get_masked(cpu_info[1], 31, 22)
-inline int32_t get_masked(int32_t val, int32_t h, int32_t l)
-{
-    val &= (0x7FFFFFFF >> (31-(h-l))) << l;
-    return val >> l;
-}
 
 class autoAdjust
 {
@@ -81,7 +74,7 @@ public:
 
                 conf += std::string("    { \"thread_mode\" : ");
                 conf += std::string(double_mode ? "2" : "1");
-                conf += std::string(", \"prefetch\" : true, \"asm\" : \"off\", \"affine_to_cpu\" : ");
+                conf += std::string(", \"prefetch\" : true, \"asm\" : \"auto\", \"affine_to_cpu\" : ");
                 conf += std::to_string(aff_id);
                 conf += std::string(" },\n");
 
@@ -142,7 +135,8 @@ private:
             L3KB_size = get_masked(cpu_info[3], 31, 18) * 512;
 
             ::jconf::cpuid(1, 0, cpu_info);
-            if(get_masked(cpu_info[0], 11, 8) < 0x17) //0x17h is Zen
+
+			if(getModel().family < 0x17) //0x17h is Zen
                 old_amd = true;
 
             return true;

xmrstak/backend/cpu/autoAdjustHwloc.hpp
@@ -69,7 +69,7 @@ public:
             {
                 conf += std::string("    { \"thread_mode\" : ");
                 conf += std::string((id & 0x8000000) != 0 ? "2" : "1");
-                conf += std::string(", \"prefetch\" : true,  \"asm\" : \"off\", \"affine_to_cpu\" : ");
+                conf += std::string(", \"prefetch\" : true,  \"asm\" : \"auto\", \"affine_to_cpu\" : ");
                 conf += std::to_string(id & 0x7FFFFFF);
                 conf += std::string(" },\n");
             }