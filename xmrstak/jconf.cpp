fireice-uk/xmr-stak

Add CryptoNight Turtle Support. Special thanks to @DaveLong for his hard work in getting this done.

xmrstak/jconf.cpp
@@ -99,6 +99,7 @@ xmrstak::coin_selection coins[] = {
 	{ "cryptonight_lite_v7", {cryptonight_aeon, cryptonight_aeon, 0u},            {cryptonight_aeon, cryptonight_aeon, 0u},     nullptr },
 	{ "cryptonight_lite_v7_xor", {cryptonight_aeon, cryptonight_ipbc, 255u},      {cryptonight_aeon, cryptonight_aeon, 0u}, nullptr },
 	{ "cryptonight_superfast",   {cryptonight_heavy, cryptonight_superfast, 255u},{cryptonight_heavy, cryptonight_superfast, 0u},   nullptr },
+	{ "cryptonight_turtle",  {cryptonight_turtle, cryptonight_turtle, 0u},      {cryptonight_turtle, cryptonight_turtle, 0u},   nullptr },
 	{ "cryptonight_v7",      {cryptonight_monero_v8, cryptonight_monero, 255u},   {cryptonight_monero_v8, cryptonight_monero_v8, 0u}, nullptr },
 	{ "cryptonight_v8",      {cryptonight_monero_v8, cryptonight_monero_v8, 255u},   {cryptonight_monero_v8, cryptonight_monero_v8, 0u}, nullptr },
 	{ "cryptonight_v7_stellite", {cryptonight_monero_v8, cryptonight_stellite, 255u}, {cryptonight_monero_v8, cryptonight_monero_v8, 0u}, nullptr },
@@ -111,7 +112,8 @@ xmrstak::coin_selection coins[] = {
 	{ "qrl",             	 {cryptonight_monero_v8, cryptonight_monero, 255u},   {cryptonight_monero_v8, cryptonight_monero_v8, 0u}, nullptr },
 	{ "ryo",                 {cryptonight_heavy, cryptonight_heavy, 0u},          {cryptonight_heavy, cryptonight_heavy, 0u},   nullptr },
 	{ "stellite",            {cryptonight_monero_v8, cryptonight_stellite, 255u}, {cryptonight_monero_v8, cryptonight_monero_v8, 0u}, nullptr },
-	{ "turtlecoin",          {cryptonight_aeon, cryptonight_aeon, 0u},            {cryptonight_aeon, cryptonight_aeon, 0u},     nullptr }
+	{ "turtlecoin",          {cryptonight_turtle, cryptonight_aeon, 5u},            {cryptonight_aeon, cryptonight_aeon, 0u},     nullptr },
+	{ "plenteum",			 {cryptonight_turtle, cryptonight_aeon, 5u},            {cryptonight_aeon, cryptonight_aeon, 0u},     nullptr }
 };
 
 constexpr size_t coin_algo_size = (sizeof(coins)/sizeof(coins[0]));

Dead2/CryptoGoblin

xmrstak/jconf.cpp
@@ -102,6 +102,7 @@ xmrstak::coin_selection coins[] = {
     { "cryptonight_lite_v7_xor",    {cryptonight_ipbc,      cryptonight_ipbc,       0u },  {cryptonight_aeon,       cryptonight_aeon,       0u },   nullptr },
     { "cryptonight_masari",         {cryptonight_masari,    cryptonight_masari,     0u },  {cryptonight_heavy,      cryptonight_heavy,      0u },   nullptr },
     { "cryptonight_superfast",      {cryptonight_superfast, cryptonight_superfast,  0u },  {cryptonight_heavy,      cryptonight_heavy,      0u },   nullptr },
+    { "cryptonight_turtle",         {cryptonight_turtle,    cryptonight_turtle,     0u },  {cryptonight_aeon,       cryptonight_aeon,       0u },   nullptr },
     { "cryptonight_v7",             {cryptonight_monero,    cryptonight_monero,     0u },  {cryptonight_monero_v8,  cryptonight_monero_v8,  0u },   nullptr },
     { "cryptonight_v7_stellite",    {cryptonight_stellite,  cryptonight_stellite,   0u },  {cryptonight_monero_v8,  cryptonight_monero_v8,  0u },   nullptr },
     { "cryptonight_v8",             {cryptonight_monero_v8, cryptonight_monero_v8,  0u },  {cryptonight_monero_v8,  cryptonight_monero_v8,  0u },   nullptr },
@@ -118,6 +119,8 @@ xmrstak::coin_selection coins[] = {
     { "qrl",                        {cryptonight_monero,    cryptonight_monero,     0u },  {cryptonight_monero_v8,  cryptonight_monero_v8,  0u },   nullptr },
     { "ryo",                        {cryptonight_heavy,     cryptonight_heavy,      2u },  {cryptonight_heavy,      cryptonight_heavy,      0u },   nullptr },
     { "stellite",                   {cryptonight_stellite,  cryptonight_stellite,   0u },  {cryptonight_monero_v8,  cryptonight_monero_v8,  0u },   nullptr },
+    { "turtlecoin",                 {cryptonight_turtle,    cryptonight_turtle,     0u },  {cryptonight_aeon,       cryptonight_aeon,       0u },   nullptr },
+    { "planteum",                   {cryptonight_turtle,    cryptonight_turtle,     0u },  {cryptonight_aeon,       cryptonight_aeon,       0u },   nullptr },
 #else
     { "cryptonight_v8",             {cryptonight_monero_v8, cryptonight_monero_v8,  0u },  {cryptonight_monero_v8,  cryptonight_monero_v8,  0u },   nullptr },
     { "monero",                     {cryptonight_monero_v8, cryptonight_monero_v8,  8u },  {cryptonight_monero_v8,  cryptonight_monero_v8,  0u },   "pool.supportxmr.com:5555" },