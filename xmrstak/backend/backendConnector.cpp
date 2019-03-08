GameKyuubi/xmr-stak

change load order for backends
If CUDA is loaded before AMD but no CUDA is available it can be happen that the embadded OpenCL code is empty.
This is only an issue if the binary is builded static on a different system.

xmrstak/backend/backendConnector.cpp
@@ -60,11 +60,29 @@ std::vector<iBackend*>* BackendConnector::thread_starter(miner_work& pWork)
 
 	std::vector<iBackend*>* pvThreads = new std::vector<iBackend*>;
 
+#ifndef CONF_NO_OPENCL
+	if(params::inst().useAMD)
+	{
+		const std::string backendName = xmrstak::params::inst().openCLVendor;
+		plugin amdplugin;
+		amdplugin.load(backendName, "xmrstak_opencl_backend");
+		std::vector<iBackend*>* amdThreads = amdplugin.startBackend(static_cast<uint32_t>(pvThreads->size()), pWork, environment::inst());
+		size_t numWorkers = 0u;
+		if(amdThreads != nullptr)
+		{
+			pvThreads->insert(std::end(*pvThreads), std::begin(*amdThreads), std::end(*amdThreads));
+			numWorkers = amdThreads->size();
+			delete amdThreads;
+		}
+		if(numWorkers == 0)
+			printer::inst()->print_msg(L0, "WARNING: backend %s (OpenCL) disabled.", backendName.c_str());
+	}
+#endif
+
 #ifndef CONF_NO_CUDA
 	if(params::inst().useNVIDIA)
 	{
 		plugin nvidiaplugin;
-		std::vector<iBackend*>* nvidiaThreads;
 		std::vector<std::string> libNames = {"xmrstak_cuda_backend_cuda10_0", "xmrstak_cuda_backend_cuda9_2", "xmrstak_cuda_backend"};
 		size_t numWorkers = 0u;
 
@@ -93,25 +111,6 @@ std::vector<iBackend*>* BackendConnector::thread_starter(miner_work& pWork)
 		}
 		if(numWorkers == 0)
 			printer::inst()->print_msg(L0, "WARNING: backend NVIDIA disabled.");
-	}
-#endif
-
-#ifndef CONF_NO_OPENCL
-	if(params::inst().useAMD)
-	{
-		const std::string backendName = xmrstak::params::inst().openCLVendor;
-		plugin amdplugin;
-		amdplugin.load(backendName, "xmrstak_opencl_backend");
-		std::vector<iBackend*>* amdThreads = amdplugin.startBackend(static_cast<uint32_t>(pvThreads->size()), pWork, environment::inst());
-		size_t numWorkers = 0u;
-		if(amdThreads != nullptr)
-		{
-			pvThreads->insert(std::end(*pvThreads), std::begin(*amdThreads), std::end(*amdThreads));
-			numWorkers = amdThreads->size();
-			delete amdThreads;
-		}
-		if(numWorkers == 0)
-			printer::inst()->print_msg(L0, "WARNING: backend %s (OpenCL) disabled.", backendName.c_str());
 	}
 #endif
 

Dead2/CryptoGoblin

xmrstak/backend/backendConnector.cpp
@@ -60,11 +60,29 @@ std::vector<iBackend*>* BackendConnector::thread_starter(miner_work& pWork)
 
     std::vector<iBackend*>* pvThreads = new std::vector<iBackend*>;
 
+#ifndef CONF_NO_OPENCL
+    if(params::inst().useAMD)
+    {
+        const std::string backendName = xmrstak::params::inst().openCLVendor;
+        plugin amdplugin;
+        amdplugin.load(backendName, "CryptoGoblin_opencl");
+        std::vector<iBackend*>* amdThreads = amdplugin.startBackend(static_cast<uint32_t>(pvThreads->size()), pWork, environment::inst());
+        size_t numWorkers = 0u;
+        if(amdThreads != nullptr)
+        {
+        pvThreads->insert(std::end(*pvThreads), std::begin(*amdThreads), std::end(*amdThreads));
+            numWorkers = amdThreads->size();
+            delete amdThreads;
+        }
+        if(numWorkers == 0)
+            printer::inst()->print_msg(L0, "WARNING: backend %s (OpenCL) disabled.", backendName.c_str());
+    }
+#endif
+
 #ifndef CONF_NO_CUDA
     if(params::inst().useNVIDIA)
     {
         plugin nvidiaplugin;
-        std::vector<iBackend*>* nvidiaThreads;
         std::vector<std::string> libNames = {"CryptoGoblin_cuda10_0", "CryptoGoblin_cuda9_2", "CryptoGoblin_cuda"};
         size_t numWorkers = 0u;
 
@@ -93,25 +111,6 @@ std::vector<iBackend*>* BackendConnector::thread_starter(miner_work& pWork)
         }
         if(numWorkers == 0)
             printer::inst()->print_msg(L0, "WARNING: backend NVIDIA disabled.");
-    }
-#endif
-
-#ifndef CONF_NO_OPENCL
-    if(params::inst().useAMD)
-    {
-        const std::string backendName = xmrstak::params::inst().openCLVendor;
-        plugin amdplugin;
-        amdplugin.load(backendName, "CryptoGoblin_opencl");
-        std::vector<iBackend*>* amdThreads = amdplugin.startBackend(static_cast<uint32_t>(pvThreads->size()), pWork, environment::inst());
-        size_t numWorkers = 0u;
-        if(amdThreads != nullptr)
-        {
-        pvThreads->insert(std::end(*pvThreads), std::begin(*amdThreads), std::end(*amdThreads));
-            numWorkers = amdThreads->size();
-            delete amdThreads;
-        }
-        if(numWorkers == 0)
-            printer::inst()->print_msg(L0, "WARNING: backend %s (OpenCL) disabled.", backendName.c_str());
     }
 #endif
 