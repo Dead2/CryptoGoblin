8abab5b5a7eb6b4a7b1f96bc1a37671d7df49319
Hans Kristian Rosbach <hk-git@circlestorm.org>
2019-03-21 19:31:46 +0100

    Update to 4.3.0

--- xmrstak/version.hpp
+++ xmrstak/version.hpp
@@ -5,7 +5,7 @@
 #include "donate-level.hpp"
 
 #define XMR_STAK_NAME "CryptoGoblin"
-#define XMR_STAK_VERSION "4.2.1"
+#define XMR_STAK_VERSION "4.3.0"
 
 extern const char ver_long[];
 extern const char ver_short[];
